71d6ef441907c48c5a4f89d394957c6d12c12cd3
Josh Hayes <hays@fb.com>
2021-09-16 10:08:10 +0100

    destructuring task

---
+++ destructuring.js
@@ -0,0 +1,36 @@
+// let teacher = {
+//     name: "Walter",
+//     subject: "Chemistry",
+// };
+// console.log(name);
+
+// let teacher = {
+//     name: "Walter",
+//     subject: "Chemistry",
+// };
+// const { name } = teacher;
+// console.log(name);
+
+// const animals = ["Red Panda", "Otter", "Raccoon"];
+// const [pet] = animals;
+// console.log(pet);
+
+// function shouldDelete({ admin, verified }) {
+//     return !admin && !verified;
+// }
+
+// let user = {
+//     id: 8732429,
+//     admin: true,
+//     verified: false,
+// };
+
+// console.log(shouldDelete(user));
+
+// const [a, b, c] = [3, 1, 4, 1, 5];
+// console.log(c);
+
+// let a = 6;
+// let b = 22;
+
+// console.log({ a, b });
